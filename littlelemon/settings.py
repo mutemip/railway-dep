5de441c5cb3bc297e04e192835899d892803f4d7
mutemip <paulmutemi2030@gmail.com>
2023-04-16 15:06:30 +0300

    static_dir

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 # The settings for static files have been updated for the Graded assessment
-STATIC_URL = 'restaurant/static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
